310e101deaebc7b62ecdaf85d2cda85811a0ab56
misxiaoti <167290446+misxiaoti@users.noreply.github.com>
2024-04-17 18:36:39 +0800

    Add files via upload

---
+++ assets/public.js
@@ -0,0 +1,14 @@
+/*
+* @Author: Marte
+* @Date:   2018-05-31 14:22:56
+* @Last Modified by:   Marte
+* @Last Modified time: 2018-05-31 14:45:12
+*/
+$(".sub").click(function() {
+    console.log(111)
+   $("#mask").show();
+});
+$(".mask_top").click(function() {
+    console.log(22)
+   $("#mask").hide();
+});
